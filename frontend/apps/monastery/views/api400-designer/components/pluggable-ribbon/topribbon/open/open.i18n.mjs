bfb2d0facba5fdebafef6eb7a945a8a353ec5a96
Ankit Saxena <ankit.saxena@deeplogictech.com>
2022-07-14 22:18:22 +0530

    APICL Parser

---
+++ frontend/apps/monastery/views/api400-designer/components/pluggable-ribbon/topribbon/open/open.i18n.mjs
@@ -0,0 +1,27 @@
+export const i18n = {
+    HELP_TEXTS: {
+        en:"Open\nOpen the file", 
+        hi: "खोलें\nफ़ाइल खोलें", 
+        ja: "開いた\nファイルを開く"
+    },
+    DESCRIPTIVE_NAME: {
+        en:"Open", 
+        hi: "खोलें", 
+        ja: "開いた"
+    },
+    NEW: {
+        en:"New", 
+        hi: "नया", 
+        ja: "新着"
+    },
+    OPEN_FROM_DISK: {
+        en:"Open from disk...", 
+        hi: "डिस्क से खोलें...", 
+        ja: "ディスクから開く..."
+    },
+    OPEN_FROM_SERVER: {
+        en:"Open from server...", 
+        hi: "सर्वर से खोलें...", 
+        ja: "サーバーから開く..."
+    }
+}
