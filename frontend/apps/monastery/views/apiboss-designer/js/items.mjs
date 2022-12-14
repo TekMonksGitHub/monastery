1a71c6c0a0c302218cf42ae24c09aee7c48eea17
Sasidhar-dlt <sasidhar.katikela@deeplogictech.com>
2022-12-14 16:13:28 +0530

    Added type of method  to the api-list

--- frontend/apps/monastery/views/apiboss-designer/js/items.mjs
+++ frontend/apps/monastery/views/apiboss-designer/js/items.mjs
@@ -11,14 +11,26 @@ function init() {
 async function getItemList() {
     try {
         const result = JSON.parse(await apiman.rest(APP_CONSTANTS.API_LIST_USER_APIS, "POST", { "data": { "path": "/apps/apiboss/admin/listuserapis", } }, true));
+        console.log(result);
         if (result.data && result.data.result) {
             const models = result.data.apis.map(apis => {
                 const d = apis.split("/"); return d[d.length - 1];
             })
-            const items = []; for (const modelName of models) items.push({
-                id: modelName,
-                img: util.resolveURL(`${MODULE_PATH}/../dialogs/model.svg`), label: modelName
+            
+            const methods = result.data.apisvalue.map(value=>{
+                const urlParams = new URLSearchParams(value.split("?")[1]);
+                // console.log(urlParams);
+                // console.log(urlParams.has('method'));
+                if(!urlParams.has('method')) return "GET";
+                else return urlParams.get('method');
+
+            })
+            console.log(methods);
+            const items = []; for (let i=0;i<models.length;i++) items.push({
+                id: models[i],
+                img: util.resolveURL(`${MODULE_PATH}/../dialogs/model.svg`), label: models[i],method:methods[i]
             });
+            console.log(items);
             return JSON.stringify(items);
         }
     }
