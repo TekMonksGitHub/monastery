59cabfbabe90efe4bd802de8dec7ff81d57d3d1f
Bhaveshdhapola <bhavesh.dhapola@deeplogictech.com>
2022-12-19 13:55:50 +0530

    developer call api parameters UI

--- frontend/apps/monastery/views/apiboss-designer/components/api-details/api-details.mjs
+++ frontend/apps/monastery/views/apiboss-designer/components/api-details/api-details.mjs
@@ -22,6 +22,7 @@
           this.classList.toggle('active')
         })
       }*/
+
       const shadowRoot = api_details.getShadowRootByHostId(element.getAttribute("id"));
       console.log(shadowRoot);
       const accordion = shadowRoot.querySelectorAll("div.container-header");
@@ -30,7 +31,82 @@
           this.classList.toggle('active')
         })
        }
+       fetchApiVal(element);
     }
+
+let randomData = {
+  "orgzip":{
+    "type": "string"
+  },
+  "destzip":{
+    "type": "string"
+  },
+  "date":{
+    "type": "string"
+  },
+  "options":{
+    "type": "string"
+  },
+  "shipment":{
+    "type":"arrayofobjects",
+    "orgzip":{
+      "type": "string"
+    },
+    "destzip":{
+      "type": "string"
+    },
+    "date":{
+      "type": "string"
+    },
+    "options":{
+      "type": "string"
+    },
+    "shipment":{
+      "type":"arrayofobjects",
+      "orgzip":{
+        "type": "string"
+      },
+      "destzip":{
+        "type": "string"
+      },
+      "date":{
+        "type": "string"
+      },
+      "options":{
+        "type": "string"
+      }
+    }
+  },
+  "tags":{
+    "type": "arrayofstrings",
+  }
+}
+
+function fetchApiVal(element){
+  const shadowRoot = api_details.getShadowRootByHostId(element.getAttribute("id"));
+  const content = shadowRoot.querySelectorAll('.content');
+console.log(shadowRoot)
+console.log(content)
+
+  for(let key in randomData){
+    console.log(key);
+    console.log(randomData[key].type)
+    let child = document.createElement('div');
+    child.id = randomData[key].type;
+    child.classList.add('input-fields');
+    child.innerHTML = `
+    <label for="My${randomData[key].type}" style="text-align: center; color: #444444;
+   margin-left: 1.2em; ">${key}</label>
+   <sub class="dataType">${randomData[key].type}</sub>
+    ${!randomData[key].type.includes("array") || !randomData[key].type.includes("array") ? `<input type="text" id="My${randomData[key].type}" class="input-text" />` : `<button onclick="monkshu_env.components["api-details"].addChild(event, this)">${randomData[key].type.includes("array") ? randomData[key].type.replace("arrayof", "") : randomData[key].type}</button>`}`
+    content[content.length-1].appendChild(child);
+  }
+}
+
+function addChild(event, element){
+  console.log(event);
+  console.log(element);
+}
 
 export const api_details = {
     trueWebComponentMode: true,elementConnected,elementRendered
